ef42e12005ac28d01fdaa64a6488e581ce10c166
blakebt <brenden.blake94@gmail.com>
2024-01-20 21:55:44 -0500

    refactored timer into its own component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,9 @@
-'use client'
-/* 
-TODO
+import Timer from "@/components/timer"
 
-extract all timer related code into a component
-
-*/
-
-import { useState, useEffect } from "react"
-
-export default function Home() {
-
-  const pomTime = 600
-  const shortRestTime = 300
-  const longRestTime = 1200
-
-  const [startTimer, setStartTimer] = useState<boolean>(false)
-  const [time, setTime] = useState<number>(3)
-
-  const minutes = Math.floor(time / 60)
-  const seconds = time - (minutes * 60)
-
-  const formattedMinutes = minutes < 10 ? '0' + minutes : minutes
-  const formattedSeconds = seconds < 10 ? '0' + seconds : seconds
-
-  const toggleTimer = () => {
-    setStartTimer(prev => !prev)
-  }
-  useEffect(() => {
-    if(startTimer) {
-      const interval = setInterval(() => {
-        time > 0 && setTime(prev => prev - 1)
-
-        time <= 1 && setStartTimer(false)
-        
-      }, 1000)
-      return () => clearInterval(interval)
-    }
-  }, [time, startTimer])
-
+export default function Pomodoro() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-orange-400">
-      <div className="flex flex-col w-1/4 h-2/6 rounded items-center justify-center bg-orange-300/50">
-        <div className="flex rounded items-center justify-center mx-auto my-5 w-full">
-          <div className="flex text-white text-9xl font-bold mb-5">{`${formattedMinutes}:${formattedSeconds}`}</div>
-        </div>
-        <button className="rounded drop-shadow-md bg-white px-5 py-2 text-orange-400 text-3xl font-bold w-1/4" onClick={toggleTimer}>{startTimer ? 'Pause' : 'Start'}</button>
-      </div>
+      <Timer state='FOCUS'/>
     </div>
     
   )
--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -1,3 +1,59 @@
-export default function Timer() {
+'use client'
+
+import { useState, useEffect } from "react"
+
+interface TimerProps {
+  state: string,
+}
+
+export default function Timer({state}: TimerProps) {
+  const [startTimer, setStartTimer] = useState<boolean>(false)
+
+  var currentTime;
+  if(state === 'FOCUS') {
+    currentTime = 600
+  } else if(state === 'SHORT') {
+    currentTime = 300
+  } else {
+    currentTime = 900
+  }
+
+  const [time, setTime] = useState<number>(currentTime)
+
   
+
+  const minutes = Math.floor(time / 60)
+  const seconds = time - (minutes * 60)
+
+  const formattedMinutes = minutes < 10 ? '0' + minutes : minutes
+  const formattedSeconds = seconds < 10 ? '0' + seconds : seconds
+
+  const toggleTimer = () => {
+    setStartTimer(prev => !prev)
+  }
+  useEffect(() => {
+    
+
+    if(startTimer) {
+      const interval = setInterval(() => {
+        time > 0 && setTime(prev => prev - 1)
+
+        if(time <= 1) {
+          alert('timer ended')
+          setStartTimer(false)
+          setTime(time) // this will be set to the next state (pom, short rest, long rest)
+        }
+      }, 1000)
+      return () => clearInterval(interval)
+    }
+  }, [time, startTimer])
+
+  return (
+    <div className="flex flex-col w-1/4 h-2/6 rounded items-center justify-center bg-orange-300/50">
+      <div className="flex rounded items-center justify-center mx-auto my-5 w-full">
+        <div className="flex text-white text-8xl font-bold mb-5">{`${formattedMinutes}:${formattedSeconds}`}</div>
+      </div>
+      <button className="rounded drop-shadow-md bg-white px-5 py-2 text-orange-400 text-3xl font-bold w-1/4" onClick={toggleTimer}>{startTimer ? 'Pause' : 'Start'}</button>
+    </div>
+  )
 }
